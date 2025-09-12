edb4d76f864446caad0de7ac9790c07eb8647432
Devendra Kashyap <dkashyap2105@gmail.com>
2025-09-12 14:02:36 +0530

    into the practice

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Car {
+    // Attributes (fields)
+    String model;
+    int year;
+
+    // Method
+    void drive() {
+        System.out.println(model + " is driving!");
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        // Creating objects of the Car class
+        Car myCar = new Car();
+        myCar.model = "Toyota";  // Set attribute
+        myCar.year = 2020;       // Set attribute
+        myCar.drive();           // Call method, outputs: Toyota is driving!
 
-        for (int i = 1; i <= 5; i++) {
-             System.out.println("i = " + i);
-        }
+
     }
 }
