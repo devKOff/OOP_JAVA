e1277a06b5781b65bdb2d6c6c108f21f793ef02a
Devendra Kashyap <dkashyap2105@gmail.com>
2025-09-13 11:18:37 +0530

    Initial object

--- src/Objects.java
+++ src/Objects.java
@@ -1,6 +1,18 @@
+
+
 public class Objects {
     public static void main(String[] args) {
-        
-        }
+
+        Car myCar = new Car();
+        System.out.println(myCar.price); // calling attributes.
+
+        // calling the method will give everything in the method.
+        myCar.Drive();
+        myCar.nDrive();
+
+        // all these method are from the Car class(Car.java)
+        // the .java file should be in same folder .
+
+
     }
 }
