ea9f93d96bac9baccd6363b1b68db7c8740d64e3
Devendra Kashyap <dkashyap2105@gmail.com>
2025-09-19 14:58:36 +0530

    modified

--- src/packages.java
+++ src/packages.java
@@ -2,6 +2,9 @@
 // interfaces, and other types into a single namespace. They help
 // manage large codebases, avoid naming conflicts,
 // and provide access control.
-
 public class packages {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
 }
