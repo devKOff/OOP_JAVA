dcd30c2cf8e3dae6463be53fc1f1266d431025fb
Devendra Kashyap <dkashyap2105@gmail.com>
2025-09-19 13:56:21 +0530

    course added

---
+++ src/packages.java
@@ -0,0 +1,7 @@
+// packages are a mechanism to organize and group related classes,
+// interfaces, and other types into a single namespace. They help
+// manage large codebases, avoid naming conflicts,
+// and provide access control.
+
+public class packages {
+}
