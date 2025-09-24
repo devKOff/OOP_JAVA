007c57142011d18a3ca1f8d0db3e06c2ac6231bb
Devendra Kashyap <dkashyap2105@gmail.com>
2025-09-24 14:41:30 +0530

    new pr added

---
+++ src/withdrawBank.java
@@ -0,0 +1,22 @@
+class bank{
+    int balance = 100000;
+
+}
+class withdraw extends bank{
+    int value;
+    withdraw(int balance) {
+        if (balance > value) {
+            System.out.println("This is bank 2");
+
+        }
+    }
+
+}
+
+
+
+public class withdrawBank {
+    public static void main(String[] args) {
+
+    }
+}
